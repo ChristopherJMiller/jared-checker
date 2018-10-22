fd57814b8c74706ab4145d3c1b99cad7803f22df
Christopher Miller <admin@mctherealm.net>
2018-10-22 17:54:13 -0400

    Modified grader to use Levenshtein distance, added results and tiers

--- src/JaredChecker.js
+++ src/JaredChecker.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import { withStyles } from '@material-ui/core/styles';
+import React, { Component } from "react"
+import TextField from "@material-ui/core/TextField"
+import Button from "@material-ui/core/Button"
+import { withStyles } from "@material-ui/core/styles"
+import Typography from '@material-ui/core/Typography';
+import Levenshtein from "levenshtein"
 
 const styles = theme => ({
   div: {
     justifyContent: "center",
     alignItems: "center",
     display: "flex",
-    flexDirection: "column"
+    flexDirection: "column",
+    textAlign: "center"
   }
-});
+})
 
 class JaredChecker extends Component {
   constructor(props) {
@@ -18,13 +21,17 @@ class JaredChecker extends Component {
     this.state = {
       compareName: "Jared",
       attemptedName: "",
-      score: 0
+      score: 0,
+      scorePosted: false
     }
   }
 
   checkSpelling = () => {
-    console.log(this.state.attemptedName)
-    let currentScore = 100;
+    let l = new Levenshtein(this.state.compareName.toLowerCase(), this.state.attemptedName.toLowerCase())
+    this.setState({
+      score: 100 - (l.distance * 20),
+      scorePosted: true
+    })
   }
 
   updateAttemptedName = (currentAttempt) => {
@@ -33,8 +40,41 @@ class JaredChecker extends Component {
     })
   }
 
+  getGrade = () => {
+    let tierStep = Math.round(this.state.score / 20)
+    switch(tierStep) {
+      case 5:
+        return "S"
+      case 4:
+        return "A"
+      case 3:
+        return "B"
+      case 2:
+        return "C"
+      case 1:
+        return "D"
+      case 0:
+        return "F"
+      default:
+        return "FF"
+    }
+  }
+
   render() {
-    const { classes } = this.props;
+    const { classes } = this.props
+    let results = null
+    if (this.state.scorePosted) {
+      results = (
+        <div>
+          <Typography component="h4" variant="h4" gutterTop>
+            This would be rated a {this.state.score} out of 100.
+          </Typography>
+          <Typography component="h4" variant="h6" gutterTop>
+            aka a {this.getGrade()} tier spelling.
+          </Typography>
+        </div>
+      )
+    }
     return (
       <div className={classes.div}>
         <TextField
@@ -47,6 +87,7 @@ class JaredChecker extends Component {
         <Button variant="contained" color="primary" onClick={this.checkSpelling}>
           Grade Spelling
         </Button>
+        { results }
       </div>
     );
   }
